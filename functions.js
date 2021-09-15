df8f2b9c6abeb5f4ecf997e9353dd4106e3b4f41
BaranyG <72973458+BaranyG@users.noreply.github.com>
2021-09-15 17:52:23 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,73 @@
+const { create } = require("domain");
+const fs = require("fs");
+const moviePath = "C:/Users/Lenovo/Desktop/Torrent/"
+const database = {
+    Paths: [],
+    Movies: [],
+    Directories: []
+};
+database.Paths.push(moviePath);
+
+module.exports = {
+    createPaths: function(){
+        for(path of database.Paths){
+            const Directories = fs.readdirSync(path)
+            .filter(dir => fs.statSync(path+dir).isDirectory());
+            for(directories of Directories){
+                if(!(database.Paths.includes(path+directories+"/")))
+                    database.Paths.push(path+directories+"/");
+            }
+        }
+    },
+
+    load_dir: function(path){
+        const movie_files = fs.readdirSync(path).filter(
+        file => file.endsWith(".mp4") || file.endsWith(".avi") || file.endsWith(".mkv"));
+    
+        for(const file of movie_files){
+            const Film = {
+                Path: path + file,
+                Name: file.slice(0, file.lastIndexOf(".")),
+                Format: file.slice(file.lastIndexOf("."), file.length)
+            }
+        database.Movies.push(Film);
+        }
+    },
+
+    createMovies: function(){
+        database.Paths.forEach(e => this.load_dir(e));
+    },
+
+    createDirectories: function(){
+        for(film of database.Movies){
+            film.Path = film.Path.toString().slice(moviePath.length);
+            let Arr = film.Path.split("/");
+            const Film = {
+                Movies: Arr.pop()
+            }
+            
+            //AZ BAAAAAJ
+            if(database.Directories.length!=0){
+                for(let i = 0; i < database.Directories.length; i++){
+                    
+                    let tempArr = [];
+                    //tempArr = database.Directories[i].filter(e => typeof e !== "object");
+                    
+                    console.log("\nTömb: \nIndex: ", database.Directories[i].length, database.Directories[i]);
+                    
+                    
+                }
+            }
+        
+            Arr.push(Film);
+            database.Directories.push(Arr);
+        }
+    },
+
+    createDatabase: function(){
+        fs.writeFile('./database.json', JSON.stringify(database, null, 4), function(err){
+            if(err)
+                console.log("Error: Writing file:", err);
+        });
+    }
+}
--- index.js
+++ index.js
@@ -3,78 +3,15 @@
 const express = require("express");
 const app = express();
 module.exports = app;
-const fs = require("fs");
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
+const func = require("./functions.js");
 const port = 8000;
-const moviePath = "C:/Users/Lenovo/Desktop/Torrent/"
 
-const database = {
-    Paths: [],
-    Movies: [],
-    Directories: []
-};
-database.Paths.push(moviePath);
-
-for(path of database.Paths){
-    const Directories = fs.readdirSync(path)
-    .filter(dir => fs.statSync(path+dir).isDirectory());
-    for(directories of Directories){
-        if(!(database.Paths.includes(path+directories+"/")))
-            database.Paths.push(path+directories+"/");
-    }
-}
-
-const load_dir = (path) => {
-    const movie_files = fs.readdirSync(path).filter(
-    file => file.endsWith(".mp4") || file.endsWith(".avi") || file.endsWith(".mkv"));
-
-    for(const file of movie_files){
-        const Film = {
-            Path: path + file,
-            Name: file.slice(0, file.lastIndexOf(".")),
-            format: file.slice(file.lastIndexOf("."), file.length)
-        }
-    database.Movies.push(Film);
-    }
-}
-
-database.Paths.forEach(e => load_dir(e));
-
-
-for(film of database.Movies){
-    film.Path = film.Path.toString().slice(moviePath.length);
-    let Arr = film.Path.split("/");
-    const Film = {
-        Movies: Arr.pop()
-    }
-    
-    //AZ BAAAAAJ
-    if(database.Directories.length!=0){
-        for(let i = 0; i < database.Directories.length; i++){
-            let tempArr = [];
-            tempArr = database.Directories[i];
-            let index1 = database.Directories[i].length;
-            let index2 = Arr.length;
-            console.log(database.Directories[i], Arr);
-            if(index1 == index2)
-                for(let j = 0; j < index1; j++)
-                    if(database.Directories[i][j] == Arr[j]);
-
-                
-                    
-        
-                
-        }
-    }
-    Arr.push(Film);
-    database.Directories.push(Arr);
-}
-
-fs.writeFile('./database.json', JSON.stringify(database, null, 4), function(err){
-    if(err)
-        console.log("Error: Writing file:", err);
-});
+func.createPaths();
+func.createMovies();
+func.createDirectories();
+func.createDatabase();
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
@@ -91,5 +28,14 @@ app.get("/video-player", function (req, res) {
 app.post("/handle-form-data", (req, res) => {
     console.log(req.body);
 });
+
+app.get("/database", function(req, res) {
+    res.sendFile(__dirname + "/database.json");
+});
+
+app.get("/style", function(req, res) {
+    res.sendFile(__dirname + "/style.css");
+});
+
 
 const video = require("./video.js")(app);
